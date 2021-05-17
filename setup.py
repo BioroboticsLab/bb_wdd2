b50e9aaa70a88f2642c1cb0e97b20af07374c1d7
David Dormagen <czapper@gmx.de>
2021-05-17 10:46:11 +0200

    setup: replaced import from pip._internal

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from distutils.core import setup
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
 
-install_reqs = parse_requirements('requirements.txt', session=False)
-reqs = [str(ir.req) for ir in install_reqs]
-dep_links = [str(req_line.url) for req_line in install_reqs]
+def parse_requirements(filename):
+    with open(filename, "r") as file:
+        lines = (line.strip() for line in file)
+        return [line for line in lines if line and not line.startswith("#")]
+
+reqs = parse_requirements("requirements.txt")
 
 setup(
     name='bb_wdd',
@@ -18,7 +17,6 @@ setup(
         ]
     },
     install_requires=reqs,
-    dependency_links=dep_links,
     extras_require={
         'Flea3': ['PyCapture2'],
     },
